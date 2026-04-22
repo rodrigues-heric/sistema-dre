9f89416fa5b76dcc4ee8a3294e25a2a6ebd92c13
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-04-22 20:13:19 -0300

    Feat: adding middleware for error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,8 @@ import coloredMorgan from "./logger";
 
 import routerDRE from "./routes/dreRoutes";
 
+import { errorHandler } from "./middlewares/errorHandler";
+
 const API_PREFIX: string = "/api/v1";
 const app: Express = express();
 
@@ -10,5 +12,7 @@ app.use(express.json());
 app.use(coloredMorgan);
 
 app.use(`${API_PREFIX}/dre`, routerDRE);
+
+app.use(errorHandler);
 
 export default app;
--- backend/src/controllers/dreController.ts
+++ backend/src/controllers/dreController.ts
@@ -3,12 +3,6 @@ import { Request, Response } from "express";
 import * as DREService from "../services/dreService";
 
 export const getTest = (request: Request, response: Response) => {
-  try {
-    const result = DREService.getData();
-    response.status(200).json(result);
-  } catch (error) {
-    response
-      .status(500)
-      .json({ error: "an error occurred while getting data" });
-  }
+  const result = DREService.getData();
+  response.status(200).json(result);
 };
