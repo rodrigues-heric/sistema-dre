19d5fe320fc6404e5976da17cc644825e05faa47
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-04-22 17:55:35 -0300

    Feat: adding logger (morgan)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
 
 const app: Express = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/test", (request: Request, response: Response) => {
   response
