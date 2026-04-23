abc9df74bf61712717544c0ddb3cc84598d6ee1f
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-04-23 13:12:20 -0300

    Test: adding app tests

--- backend/src/app.test.ts
+++ backend/src/app.test.ts
@@ -1,4 +1,16 @@
 import request from "supertest";
 import app from "./app";
 
-describe("API", () => {});
+const API_PREFIX: string = "/api/v1";
+
+describe("API start", () => {
+  it("Should return 404 for a not exiting route", async () => {
+    const result = await request(app).get("/random-route");
+    expect(result.status).toBe(404);
+  });
+
+  it("Should access dre-rentabilidade successfully", async () => {
+    const result = await request(app).get(`${API_PREFIX}/dre-rentabilidade`);
+    expect(result.status).not.toBe(500);
+  });
+});
